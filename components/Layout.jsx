cc5c50ce8732507036c1f6cb156602a0f8635428
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-30 17:13:38 -0600

    layout principal

---
+++ components/Layout.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+/**
+ * Layout
+ * Los archivos de layout en NextJS hasta el momento (versipon 13) siguen siendo componentes de React
+ * Para futuras versiones, el equipo de NextJS considera una carpeta llamada app donde funcionarán al clásico estilo de Remix
+ *      archivo.jsx (layout) - carpeta con nombre igual al archivo - contenido interno de la carpeta a proyectar en el lauout
+ */
+const Layout = ({children, title = '', description = ''}) => {
+  return (
+    <div>
+        {/* Definir información de cabecera para todas las páginas que hagan uso de este Layout */}
+        <Head>
+            <title>{`GuitarLA - ${title}`}</title>
+            <meta name="description" content={description} />
+        </Head>
+        {children}
+    </div>
+  )
+}
+
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+import Layout from '@/components/Layout'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <Layout title='Home' description='GuitarLA, el mejor lugar para comprar una guitarra'>
       <h1>GuitarLA - NextJS</h1>
-    </div>
+    </Layout>
   )
 }
