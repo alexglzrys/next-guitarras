e3891f4e1cec70b36e706f25746cbb5705622161
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-30 18:49:11 -0600

    aplicar estilo CSS al Header y Navegación

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,27 @@
-import Image from "next/image"
-import Navegacion from "./Navegacion"
+import Image from "next/image";
+import Navegacion from "./Navegacion";
+
+// NextJS recomienda en la medida de lo posible trabajar con modulos de CSS
+// Sin embarga, los demás paradigmas de CSS en React se pueden seguir usando (css global, styled components, etc)
+import styles from "../styles/Header.module.css";
 
 // Podemos importar las imagenes, o hacer uso directo de ellas si se encuentran en la carpeta public
 // import Logotipo from '../public/img/logo.svg'
 
 const Header = () => {
   return (
-    <header>
-        <Image src='img/logo.svg' height={100} width={300} alt="Logotipo principal" />
+    <header className={styles.header}>
+      <div className={`contenedor ${styles.barra}`}>
+        <Image
+          src="img/logo.svg"
+          height={100}
+          width={300}
+          alt="Logotipo principal"
+        />
         <Navegacion />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- components/Navegacion.jsx
+++ components/Navegacion.jsx
@@ -1,15 +1,20 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import styles from "../styles/Navegacion.module.css";
 
 const Navegacion = () => {
   return (
-    <nav>
-        <Link href='/'>Incio</Link>
-        <Link href='/nosotros'>Nosotros</Link>
-        <Link href='/blog'>Blog</Link>
-        <Link href='/tienda'>Tienda</Link>
+    <nav className={styles.navegacion}>
+      {/* 
+      Si deseamos especificar una clase de css al componente Link, es necesario usarlo como wrapper, 
+      y declarar una etiqueta <a></a> como hijo, y declarar la clase de css sobre el mismo
+      El mejor enfoque es que en el CSS se apunte al padre "navegacion" y posterior a ello apuntar al hijo "a"
+      */}
+      <Link href="/">Incio</Link>
+      <Link href="/nosotros">Nosotros</Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/tienda">Tienda</Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Navegacion
+export default Navegacion;
